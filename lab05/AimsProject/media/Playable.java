070ad39d2f33f179ace1894dbe0d2f954216253f
Nguyễn Nhật Minh <165288949+NhMinh-2610@users.noreply.github.com>
2024-12-17 00:12:17 +0700

    Delete lab05 directory

--- lab05/AimsProject/media/Playable.java
+++
@@ -1,8 +0,0 @@
-package lab05.AimsProject.media;
-//NguyenNhatMinh20225886
-import lab05.AimsProject.exception.PlayerException;
-
-public interface Playable {
-    // Method to play
-    public void play() throws PlayerException;
-}
